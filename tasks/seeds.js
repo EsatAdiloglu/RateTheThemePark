34ada9f2a6410d633039d2a5e48cd1ee7990f90c
EsatAdiloglu <esatadiloglu16@gmail.com>
2024-12-02 12:47:40 -0500

    did package.json

--- tasks/seeds.js
+++ tasks/seeds.js
@@ -11,20 +11,20 @@ let themepark3 = undefined;
 
 try{
     //1
-    themepark1 = await themeparksfunc.createTeam("Disneyland", "1313 DisneyLand Drive", "Anaheim", "California", "United States")
+    themepark1 = await themeparksfunc.createThemePark("Disneyland", "1313 DisneyLand Drive", "Anaheim", "California", "United States")
 }
 catch(e){
     console.log(e);
 }
 try{
 
-    themepark2 = await themeparksfunc.createTeam("Adventure City", "1238 South Beach Blvd", "San Francsico", "California", "United States")
+    themepark2 = await themeparksfunc.createThemePark("Adventure City", "1238 South Beach Blvd", "San Francsico", "California", "United States")
 }
 catch(e){
     console.log(e);
 }
 try{
-    themepark3 = await themeparksfunc.createTeam("Adventureland", "I-80 & Highway 65", "Altoona", "Iowa", "United States")
+    themepark3 = await themeparksfunc.createThemePark("Adventureland", "I-80 & Highway 65", "Altoona", "Iowa", "United States")
 }
 catch(e){
     console.log(e);
