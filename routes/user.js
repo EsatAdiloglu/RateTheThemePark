6ec9db897fe4598a5823447022dd92b622c502ae
Ryan Eshan <ryaneshan04@gmail.com>
2024-12-02 16:12:02 -0500

    work in progress user.js

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,23 @@
 import {Router} from "express"
 const router = Router();
+import bcrypt from 'bcryptjs';
+
+router.get('/', async (req, res) => {
+    res.json({route: '/users', method: req.method});
+  });
+  
+  router.post('/', async (req, res) => {
+    res.json({route: '/users', method: req.method});
+  });
+  
+  router.post('/login', async (req, res) => {
+    req.session.user = {firstName: 'Patrick', lastName: 'Hill', userId: 123};
+    res.redirect('/private');
+  });
+  
+  router.get('/logout', async (req, res) => {
+    req.session.destroy();
+    res.send('Logged out');
+  });
 
 export default router;
