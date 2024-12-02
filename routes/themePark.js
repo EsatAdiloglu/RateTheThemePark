8359689531ead3fb0683ff7ad3f4685a51fb021c
Ryan Eshan <ryaneshan04@gmail.com>
2024-12-02 16:12:02 -0500

    work in progress user.js

--- routes/themePark.js
+++ routes/themePark.js
@@ -10,6 +10,7 @@ router.route('/').get(async (req, res)  => {
         if (!name) throw "Error: You must provide a theme park name to search";
 
         const parks = await themeParkData.getThemeParksByName(name);
+
         if (parks.length === 0) {
             return res.status(404).json({ message: `No theme parks found with name '${name}'` });
         }
@@ -25,13 +26,22 @@ router.route('/listofthemeparks').post(async (req, res) => {
     try {
          const themeParkInput = req.body.themeParkInput;
 
+        //  const {name} = req.body;
+        // if (!name) throw "Error: You must provide a theme park name to search.";
+
         if (!name || !streetaddress || !city || !country || !state) {
             throw "Error: Missing required fields. Ensure all fields (name, streetaddress, city, country, state) are provided.";
         }
-
+    
         const newThemePark = await getThemeParksByName(themeParkInput);
-        res.render("listOfThemeParks", { parks: newThemePark })
-        res.status(200).json(newThemePark); 
+
+        // const parks = await themeParkData.getThemeParksByName(name);
+        
+        // if (parks.length === 0) {
+        //     return res.status(404).render("listOfThemeParks", { message: `No theme parks found with name '${name}'`});
+        // }
+
+        res.status(200).render("listOfThemeParks", { parks }) 
     } catch (e) {
         res.status(400).json({error: e});
     }
